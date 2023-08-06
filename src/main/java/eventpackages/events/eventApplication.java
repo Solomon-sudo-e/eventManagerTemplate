5a600a0b6d66eed6bcfb57b930a590592f6e8ecc
Solomon Hufford <solo.hufford@gmail.com>
2023-08-06 03:21:13 -0500

    Added debug agent
    Added debug agent through project reactor tools after reading some documentation. This was the easiest solution without sacrificing runtime using something like a Hook.onOperatorDebug() or a bunch of checkpoints.

--- src/main/java/eventpackages/events/eventApplication.java
+++ src/main/java/eventpackages/events/eventApplication.java
@@ -3,11 +3,14 @@ package eventpackages.events;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import eventpackages.ComponentScanMarker;
+import reactor.core.publisher.Hooks;
+import reactor.tools.agent.ReactorDebugAgent;
 
 @SpringBootApplication(scanBasePackageClasses = {ComponentScanMarker.class})
 public class eventApplication {
 
 	public static void main(String[] args) {
+		ReactorDebugAgent.init();
 		SpringApplication.run(eventApplication.class, args);
 	}
 
